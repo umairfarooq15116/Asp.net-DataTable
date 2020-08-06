62a9643da74126b096d136d4a375a81a5c9e796d
umairfarooq15116 <umair.pucit16@gmail.com>
2020-08-06 15:44:31 +0500

    first commit

---
+++ DataTable/Jquery/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Jquery.Models;
+using System.Web.Mvc;
+
+namespace Jquery.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeDBEntities DB = new EmployeeDBEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = DB.Students.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ DataTable/Jquery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jquery.MvcApplication" Language="C#" %>
